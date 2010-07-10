29ba1bb421b1c7a6425f720848951f8bf7abea9c
Nathaniel Flath <flat0103@gmail.com>
2010-07-10 18:10:31 -0400

    Fixed header issue for ELPA

--- mv-shell.el
+++ mv-shell.el
@@ -9,12 +9,13 @@
 ;; This file is not part of GNU Emacs.
 
 ;;; Commentary:
+
 ;; mv-shell integrates with shell-mode in order to keep buffers in sync when
 ;; moving files around.  If you enter a 'mv' command on a file that has a buffer opened,
 ;; the buffer is also renamed and moved to the location the file is moved to.  Buffers are
 ;; also moved when a directory they are in is moved.
 
-;;; Installation
+;;; Installation:
 
 ;; To install, put this file somewhere in your load-path and add the following
 ;; to your .emacs file:
